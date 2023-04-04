8fe0f51be4c757e2a30cfe75feae7d195498de06
sambunarayan <soyou30257@gmail.com>
2023-04-04 21:49:11 +0900

    Move Meta data to _app.js

--- components/header.js
+++ components/header.js
@@ -7,7 +7,6 @@ import PageHead from '@/components/PageHead';
 export default function Header() {
     return (
         <>
-            <PageHead/>
             <header className="text-gray-600 body-font">
                 <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                     <Link href="/" legacyBehavior>
